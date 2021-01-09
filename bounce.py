c60af3b1d3c8a7de09fbd3997bc55bf96b775cde
Juan Carlos Carlos <juank153021@gmail.com>
2021-01-09 00:38:15 +0000

    Bounce with a loop for

--- bounce.py
+++ bounce.py
@@ -1,29 +1,10 @@
 h1 = 100
 bounce = 0.6
 
-r1 = (h1 * bounce)
-r2 = (r1 * bounce)
-r3 = (r2 * bounce)
-r4 = (r3 * bounce)
-r5 = (r4 * bounce)
-r6 = (r5 * bounce)
-r7 = (r6 * bounce)
-r8 = (r7 * bounce)
-r9 = (r8 * bounce)
-r10 = (r9 * bounce)
+def calcular_rebote(h1,bounce):
+    return round(h1*bounce,4)
 
-print(round(r1,))
-print(round(r2,))
-print(round(r3,4))
-print(round(r4,4))
-print(round(r5,4))
-print(round(r6,4))
-print(round(r7,4))
-print(round(r8,4))
-print(round(r9,4))
-print(round(r10,4))
-
-
-
-
-
+for i in range(10):
+    h1 = calcular_rebote(h1,bounce)
+    print(h1)
+   
